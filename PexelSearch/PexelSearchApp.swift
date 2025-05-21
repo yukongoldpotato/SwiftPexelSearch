d8a696a43483d3519ed8f23a8240f71c19376980
Kazuki Minami <wakarumaji3@gmail.com>
2025-05-21 18:33:59 +0900

    add model container availablility across app

--- PexelSearch/PexelSearchApp.swift
+++ PexelSearch/PexelSearchApp.swift
@@ -12,6 +12,7 @@ struct PexelSearchApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: SavedPhoto.self)
         }
     }
 }
