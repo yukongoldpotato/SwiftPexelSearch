b5b4b753d10ea4c83d3b0855e8daee3a07694366
Kazuki Minami <wakarumaji3@gmail.com>
2025-05-21 18:32:22 +0900

    add saved images to saved tab view
    test with one image to confirm behavior
    add more images later in todo

--- PexelSearch/SavedView.swift
+++ PexelSearch/SavedView.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SavedView: View {
+    @Query var saved: [SavedPhoto]
+
     var body: some View {
         NavigationStack {
+            //TODO: enable saving multiple images later
+            if let data = saved.first?.imageData,
+               let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
             Text("Saved items will appear here.")
                 .navigationTitle("Saved")
         }
