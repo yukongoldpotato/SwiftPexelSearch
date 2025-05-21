beb02ac86bf349d3db73f1b4d77404efc40c32a1
Kazuki Minami <wakarumaji3@gmail.com>
2025-05-21 14:43:19 +0900

    Add PhotoSearcher to photo search
    Implement PhotoSearcher
    - @Observable class to track searchText and photos
    - performSearch() using PexelsAPIService.searchPhotos(query:)

---
+++ PexelSearch/PhotoSearcher.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoSearcher.swift
+//  PexelSearch
+//
+//  Created by Kazuki Minami on 2025/05/21.
+//
+
+import Foundation
+
+@Observable
+class PhotoSearcher {
+    var searchText: String = ""
+    var photos: [Photo] = []
+
+    private let apiService = PexelsAPIService()
+
+    func performSearch() {
+        Task {
+            do {
+                let response = try await apiService.searchPhotos(query: searchText)
+                self.photos = response.photos
+            }
+        }
+    }
+}
