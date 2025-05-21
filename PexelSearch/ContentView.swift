02e3ac5a965d427b13216d0106af9b076339f91d
Kazuki Minami <wakarumaji3@gmail.com>
2025-05-21 11:47:44 +0900

    Initial Commit

---
+++ PexelSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PexelSearch
+//
+//  Created by Kazuki Minami on 2025/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PexelSearch/PexelSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelSearchApp.swift
+//  PexelSearch
+//
+//  Created by Kazuki Minami on 2025/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PexelSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
