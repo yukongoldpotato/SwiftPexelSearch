50dea61d19554ac02cede33b29f794dc24e786a7
Kazuki Minami <wakarumaji3@gmail.com>
2025-05-28 12:55:32 +0900

    Remove fatal error, replace with error Prevent app crashing

--- PexelSearch/APIKeyManager.swift
+++ PexelSearch/APIKeyManager.swift
@@ -9,13 +9,9 @@ import Foundation
 
 enum APIKeyManager {
     static var pexelsAPIKey: String? {
-        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "PexelsAPIKey") as? String else {
-            fatalError("PexelsAPIKey not found in Info.plist. Make sure it's set in your xcconfig file.")
-        }
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "PexelsAPIKey") as? String else { return nil }
         // Confirm that its not the placeholder
-        if apiKey == "$(PEXELS_API_KEY)" || apiKey.isEmpty {
-             fatalError("PEXELS_API_KEY was not replaced during build. Check xcconfig setup.")
-        }
+        if apiKey == "$(PEXELS_API_KEY)" || apiKey.isEmpty { return nil }
         return apiKey
     }
 }
